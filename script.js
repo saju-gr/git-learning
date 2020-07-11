160891bbd74009462112e130d4afe8355d7779a6
Saju GR <sajugr.gopalakrishnan@gmail.com>
2020-07-11 12:25:28 +0530

    Files are been edited

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 <script type="text/javascript">  
-document.write("JavaScript is a simple language for javatpoint learners");  
+document.write("Javascript is a simple language for javatpoint learners");  
 </script>  
